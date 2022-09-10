06a2e11cb19f8065395ac8507d1314246f96c5bd
Glen Wilkin <glen@luthansolutions.com>
2022-09-10 15:41:17 +0100

    Add panels and cards

---
+++ ContractsAndJobs/Components/MyComponent.razor.cs
@@ -0,0 +1,65 @@
+﻿using ContractsAndJobs.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace ContractsAndJobs.Components;
+
+public partial class MyComponent
+{
+    [Parameter]
+    public int ItemId { get; set; }
+
+    [Parameter]
+    public string? ItemName { get; set; }
+
+    [Parameter]
+    public DateTime? Dob { get; set; }
+
+    [Parameter]
+    public Contact? Contact { get; set; }
+
+    private int itemId;
+    private string? itemName;
+    private DateTime? dob;
+    private Contact? contact;
+    private bool shouldRender = true;
+
+    public (int Id, string? Name) Item { get; set; }
+
+    protected override void OnInitialized()
+    {
+        base.OnInitialized();
+    }
+
+    protected override void OnParametersSet()
+    {
+        this.shouldRender = this.CheckShouldRenderAndSetPropertyFields();
+        if (this.shouldRender)
+        {
+            base.OnParametersSet();
+        }
+
+    }
+
+    private bool CheckShouldRenderAndSetPropertyFields()
+    {
+        var retVal = this.ItemId != this.itemId 
+            || this.ItemName != this.itemName 
+            || this.Dob != this.dob 
+            || this.Contact?.Id != this.contact?.Id;
+        if (retVal)
+        {
+            this.itemId = this.ItemId;
+            this.itemName = this.ItemName;
+            this.dob = this.Dob;
+            this.contact = this.Contact;
+        }
+        return retVal;
+    }
+
+    protected override bool ShouldRender() => this.shouldRender;
+
+    protected override void OnAfterRender(bool firstRender)
+    {
+        base.OnAfterRender(firstRender);
+    }
+}
---
+++ ContractsAndJobs/Pages/Dashboard.razor.cs
@@ -0,0 +1,33 @@
+﻿namespace ContractsAndJobs.Pages
+{
+    public partial class Dashboard
+    {
+        public class PopulationReport
+        {
+            public int Year { get; set; }
+            public int Population { get; set; }
+        };
+        private List<PopulationReport> PopulationData = new List<PopulationReport> {
+        new  PopulationReport { Year= 2005, Population= 10090440 },
+        new  PopulationReport { Year= 2006, Population= 20264080 },
+        new  PopulationReport { Year= 2007, Population= 2043418 },
+        new  PopulationReport { Year= 2008, Population= 11007310 },
+        new  PopulationReport { Year= 2009, Population= 21262640 },
+        new  PopulationReport { Year= 2010, Population= 2151575 },
+        new  PopulationReport { Year= 2011, Population= 21766710 },
+        new  PopulationReport { Year= 2012, Population= 12015580 },
+        new  PopulationReport { Year= 2013, Population= 22262500 },
+        new  PopulationReport { Year= 2014, Population= 2250762 }
+    };
+        private string cardContent = @"
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer magna enim, consequat iaculis lobortis malesuada, aliquet eu lorem. Integer pretium vehicula tellus non suscipit. Aliquam erat volutpat. 
+Pellentesque nunc felis, sollicitudin quis neque sit amet, interdum semper nulla. Aliquam non ex ut leo scelerisque commodo. Nunc enim mi, elementum id nisi quis, elementum ultrices arcu. Mauris rutrum metus sem, vitae accumsan turpis commodo egestas. Nulla facilisi. Ut at convallis orci. Vivamus ac commodo dui. Sed vehicula vestibulum est at tincidunt. Cras mauris lorem, fermentum ac bibendum et, ullamcorper ac risus. Vivamus in nibh pretium, tristique felis ut, tempor purus. Aliquam vulputate efficitur imperdiet. Suspendisse ut laoreet leo. Aliquam erat volutpat. Vivamus sit amet maximus massa. 
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer magna enim, consequat iaculis lobortis malesuada, aliquet eu lorem. Integer pretium vehicula tellus non suscipit. Aliquam erat volutpat. 
+Pellentesque nunc felis, sollicitudin quis neque sit amet, interdum semper nulla. Aliquam non ex ut leo scelerisque commodo. Nunc enim mi, elementum id nisi quis, elementum ultrices arcu. Mauris rutrum metus sem, vitae accumsan turpis commodo egestas. Nulla facilisi. Ut at convallis orci. Vivamus ac commodo dui. Sed vehicula vestibulum est at tincidunt. Cras mauris lorem, fermentum ac bibendum et, ullamcorper ac risus. Vivamus in nibh pretium, tristique felis ut, tempor purus. Aliquam vulputate efficitur imperdiet. Suspendisse ut laoreet leo. Aliquam erat volutpat. Vivamus sit amet maximus massa.
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer magna enim, consequat iaculis lobortis malesuada, aliquet eu lorem. Integer pretium vehicula tellus non suscipit. Aliquam erat volutpat. 
+Pellentesque nunc felis, sollicitudin quis neque sit amet, interdum semper nulla. Aliquam non ex ut leo scelerisque commodo. Nunc enim mi, elementum id nisi quis, elementum ultrices arcu. Mauris rutrum metus sem, vitae accumsan turpis commodo egestas. Nulla facilisi. Ut at convallis orci. Vivamus ac commodo dui. Sed vehicula vestibulum est at tincidunt. Cras mauris lorem, fermentum ac bibendum et, ullamcorper ac risus. Vivamus in nibh pretium, tristique felis ut, tempor purus. Aliquam vulputate efficitur imperdiet. Suspendisse ut laoreet leo. Aliquam erat volutpat. Vivamus sit amet maximus massa. 
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer magna enim, consequat iaculis lobortis malesuada, aliquet eu lorem. Integer pretium vehicula tellus non suscipit. Aliquam erat volutpat. 
+Pellentesque nunc felis, sollicitudin quis neque sit amet, interdum semper nulla. Aliquam non ex ut leo scelerisque commodo. Nunc enim mi, elementum id nisi quis, elementum ultrices arcu. Mauris rutrum metus sem, vitae accumsan turpis commodo egestas. Nulla facilisi. Ut at convallis orci. Vivamus ac commodo dui. Sed vehicula vestibulum est at tincidunt. Cras mauris lorem, fermentum ac bibendum et, ullamcorper ac risus. Vivamus in nibh pretium, tristique felis ut, tempor purus. Aliquam vulputate efficitur imperdiet. Suspendisse ut laoreet leo. Aliquam erat volutpat. Vivamus sit amet maximus massa.";
+
+    }
+}
