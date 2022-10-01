c9b4ccdea7f930e86c667d4604eb663e4c9f48c8
Glen Wilkin <glen@luthansolutions.com>
2022-10-01 12:53:17 +0100

    Drag and drop

---
+++ ContractsAndJobs/Pages/DragAndDrop.razor.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace ContractsAndJobs.Pages;
+
+public partial class DragAndDrop
+{
+    [Inject]
+    IJSRuntime? JSRuntime { get; set; }
+
+    string message = string.Empty;
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (firstRender)
+        {
+            await this.JSRuntime!.InvokeVoidAsync("import", "./js/dragdrop.js");
+        }
+        await base.OnAfterRenderAsync(firstRender);
+    }
+
+    private void OnButtonClicked(string message)
+    {
+        this.message = message;
+    }
+}
