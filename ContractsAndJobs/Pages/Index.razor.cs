771ceb8cc6e6fb69c9b7e8f29639d2392c2a7490
Glen Wilkin <glen@luthansolutions.com>
2022-10-08 11:09:02 +0100

    Dialog service

--- ContractsAndJobs/Pages/Index.razor.cs
+++ ContractsAndJobs/Pages/Index.razor.cs
@@ -2,6 +2,7 @@
 using ContractsAndJobs.ViewModels;
 using Microsoft.AspNetCore.Components;
 using Syncfusion.Blazor.DropDowns;
+using Syncfusion.Blazor.Popups;
 
 namespace ContractsAndJobs.Pages;
 
@@ -10,14 +11,27 @@ public partial class Index
     [Inject]
     private IIndexViewModel? ViewModel { get; set; }
 
+    [Inject]
+    SfDialogService? DialogService { get; set; }
+
     protected override async Task OnInitializedAsync()
     {
         await this.ViewModel!.InitialiseViewModelAsync();
     }
 
-
     private async Task SelectedPersonChangeHandlerAsync(ChangeEventArgs<string, Contact> args)
     {
         await this.ViewModel!.PopulateContactAsync(args.ItemData.Id);
     }
+
+    private async Task ShowAlert() => 
+        await DialogService!.AlertAsync(
+            "This is the alert content", 
+            "Title",
+            new DialogOptions
+            {
+                Width = "20rem",
+                Height = "15rem",
+                CssClass = "customClass "
+            });
 }
--- ContractsAndJobs/Program.cs
+++ ContractsAndJobs/Program.cs
@@ -1,6 +1,7 @@
 ﻿using ContractsAndJobs.Data;
 using ContractsAndJobs.ViewModels;
 using Syncfusion.Blazor;
+using Syncfusion.Blazor.Popups;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +14,9 @@ builder.Services.AddSingleton<WeatherForecastService>();
 builder.Services.AddTransient<IIndexViewModel, IndexViewModel>();
 builder.Services.AddTransient<IContractsAndJobsDataService, ContractsAndJobsDataService>();
 builder.Services.AddTransient<IAddContactViewModel, AddContactViewModel>();
+
+builder.Services.AddScoped<SfDialogService>();
+
 
 Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Njc3ODQ0QDMyMzAyZTMyMmUzMFJDcDhWZXRpdzlIalpFNVA3cGtSTzVkT0F5Q1RkWWtKVlUwcTdJRVVsVTg9");
 
