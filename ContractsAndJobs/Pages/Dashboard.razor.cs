299771e81fb448dfa5695dc1d9845a749956da21
Glen Wilkin <glen@luthansolutions.com>
2022-12-10 12:35:41 +0000

    Task.WaitAll

--- ContractsAndJobs/Pages/Dashboard.razor.cs
+++ ContractsAndJobs/Pages/Dashboard.razor.cs
@@ -39,11 +39,16 @@ namespace ContractsAndJobs.Pages
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer magna enim, consequat iaculis lobortis malesuada, aliquet eu lorem. Integer pretium vehicula tellus non suscipit. Aliquam erat volutpat. 
             Pellentesque nunc felis, sollicitudin quis neque sit amet, interdum semper nulla. Aliquam non ex ut leo scelerisque commodo. Nunc enim mi, elementum id nisi quis, elementum ultrices arcu. Mauris rutrum metus sem, vitae accumsan turpis commodo egestas. Nulla facilisi. Ut at convallis orci. Vivamus ac commodo dui. Sed vehicula vestibulum est at tincidunt. Cras mauris lorem, fermentum ac bibendum et, ullamcorper ac risus. Vivamus in nibh pretium, tristique felis ut, tempor purus. Aliquam vulputate efficitur imperdiet. Suspendisse ut laoreet leo. Aliquam erat volutpat. Vivamus sit amet maximus massa.";
 
-        private async Task OnButtonClicked()
+        private static List<Task> Tasks = new ();
+        private void OnButtonClicked()
         {
-            //await BrowserService!.ShowAlertMessage("Button Clicked!");
-            //var confirm = await BrowserService!.GetConfirmation("Are you sure you want to do this?");
-            //var input = await BrowserService!.GetUserInput("What is your full name?");
+            Tasks.Add(Task.Run(() => DoSomethingAsync(1, 2)));
+            Tasks.Add(Task.Run(() => DoSomethingAsync(2, 1)));
+            Tasks.Add(Task.Run(() => DoSomethingAsync(3, 2)));
+            Tasks.Add(Task.Run(() => DoSomethingAsync(4, 2)));
+            Tasks.Add(Task.Run(() => DoSomethingAsync(5, 1)));
+            Task.WaitAll(Tasks.ToArray());
+
             this.ToastService!.ShowToast(new ToastOption()
             {
                 Title = "Toast Title",
@@ -51,6 +56,22 @@ namespace ContractsAndJobs.Pages
                 ToastPosition = ToastPositions.BottomRight
             });
         }
+
+        private async Task DoSomethingAsync(int runNumber, int secondsToWait)
+        {
+            this.ToastService!.ShowToast(new ToastOption()
+            {
+                Title = $"Task Starting: {runNumber}",
+                Content = $"Task duration: {secondsToWait} seconds",
+                ToastPosition = ToastPositions.BottomRight
+            });
+            await Task.Delay(1000 * secondsToWait);
+            this.ToastService!.ShowToast(new ToastOption()
+            {
+                Title = $"Task Finished: {runNumber}",
+                Content = $"Task duration: {secondsToWait} seconds",
+                ToastPosition = ToastPositions.BottomRight
+            });        }
 
     }
 }
