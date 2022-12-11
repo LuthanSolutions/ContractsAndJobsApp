59603088c6e0e7cab3459bccbcb293c785470273
Glen Wilkin <glen@luthansolutions.com>
2022-12-11 11:39:55 +0000

    Countries and regions

---
+++ ContractsAndJobs.Models/Region.cs
@@ -0,0 +1,9 @@
+﻿namespace ContractsAndJobs.Models
+{
+    public class Region
+    {
+        public int Id { get; set; }
+        public int CountryId { get; set; }
+        public string? Name { get; set; }
+    }
+}
---
+++ ContractsAndJobs/Components/CountriesAndRegions.razor.cs
@@ -0,0 +1,44 @@
+﻿using ContractsAndJobs.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace ContractsAndJobs.Components
+{
+    public partial class CountriesAndRegions
+    {
+        private Country? SelectedCountry { get; set; }
+
+        private Region? SelectedRegion { get; set; }
+
+        [Parameter]
+        public List<Country>? Countries { get; set; }
+
+        [Parameter]
+        public List<Region>? Regions { get; set; }
+
+        private List<Region>? RegionsToShow
+        {
+            get
+            {
+                return SelectedCountry == null ?
+                    Regions :
+                    Regions?.Where(region => region.CountryId == SelectedCountry!.Id).ToList();
+            }
+        }
+
+        private List<Country>? CountriesToShow
+        {
+            get
+            {
+                var countriesToShow = SelectedRegion == null ?
+                    Countries :
+                    Countries?.Where(country => country.Id == SelectedRegion!.CountryId).ToList();
+                if(countriesToShow?.Count == 1)
+                {
+                    SelectedCountry = countriesToShow[0];
+                }
+                return countriesToShow;
+            }
+        }
+
+    }
+}
